4b21eb642808748a66037f0c1e6a174f4c774a6b
Kyle Chin <kyle.chin@sterlingcapitalbrokers.com>
2018-03-09 13:59:24 -0500

    adding in favicon job, working on callbacks

--- index.js
+++ index.js
@@ -8,26 +8,44 @@ var bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient
 //password file
 var config = require('./config');
+
 var path = require ('path');
 
 const port = process.env.PORT || 8080;
 
 //var Handlebars = require('handlebars');
 
+var environment = process.env.NODE_ENV || "test"
+
+console.log("ENVIRONMENT: " + environment)
+//local vs production
+if (process.env.NODE_ENV == "production"){
+	mongo_user = process.env.MONGO_USER;
+	mongo_pw = process.env.MONGO_PW;
+	mongo_db = process.env.MONGO_DB;
+
+} else {
+	mongo_user = config.mongodb.user_name;
+	mongo_pw = config.mongodb.user_pass;
+	mongo_db = config.mongodb.db_name;
+}
+
+
+
+
 //bodyParser
 //app.use(bodyParser.urlencoded({extended: true}))
 
 //mongodb
-var mongo_link = "mongodb://"+process.env.MONGO_USER+":"
-	+process.env.MONGO_PW+"@ds151070.mlab.com:51070/startpage-test";
+var mongo_link = "mongodb://"+mongo_user+":"
+	+mongo_pw+"@ds151070.mlab.com:51070/startpage-test";
 
 console.log("mongoDB link: " +mongo_link)	
 
-
-MongoClient.connect(mongo_link, (err, client) =>{
+MongoClient.connect(mongo_link, (err, client)=>{
 	if (err) return console.log(err)
-		db = client.db(process.eng.MONGO_DB)
-		console.log("Connected to MongoDB")
+	db = client.db(mongo_db);
+	console.log("Connected to MongoDB");
 
 	app.use(bodyParser.json());
 	app.use(bodyParser.urlencoded({extended: true}));
@@ -60,7 +78,8 @@ MongoClient.connect(mongo_link, (err, client) =>{
 
 		db.collection("bookmarks").find().toArray(function(err, result){{
 			if(err) throw err;
-			res.render('pages/main', {book_in: result})
+			console.log("Pulled bookmarks");
+			res.render('pages/main', {book_in: result});
 		}});
 
 		// res.render('partials/bookmarks_list', { book_in: db.collection("bookmarks").find({}).toArray(function(err, result){
--- jobs/pull_favicons.js
+++ jobs/pull_favicons.js
@@ -1,6 +1,75 @@
 var MongoClient = require('mongodb').MongoClient
-var config = require('./config');
-var mongo_link = "mongodb://"+process.env.MONGO_USER+":"
-	+process.env.MONGO_PW+"@ds151070.mlab.com:51070/startpage-test";
+var config = require('../config');
+
+
+var rp = require('request-promise');
+var cheerio = require('cheerio');
+
+var environment = process.env.NODE_ENV || "test";
+
+console.log(environment);
+
+if (environment == "production"){
+	mongo_user = process.env.MONGO_USER;
+	mongo_pw = process.env.MONGO_PW;
+	mongo_db = process.env.MONGO_DB;
+
+} else {
+	mongo_user = config.mongodb.user_name;
+	mongo_pw = config.mongodb.user_pass;
+	mongo_db = config.mongodb.db_name;
+}
+
+
+var mongo_link = "mongodb://"+mongo_user+":"
+	+mongo_pw+"@ds151070.mlab.com:51070/startpage-test";
 
 console.log("mongoDB link: " +mongo_link)	
+
+MongoClient.connect(mongo_link, (err, client)=>{
+	if (err) return console.log(err)
+	db = client.db(mongo_db);
+	
+	console.log("Connected to MongoDB");
+
+	//scrape each site in database collection for favicons
+	//get all bookmarks
+	var bm_list;
+	db.collection("bookmarks").find().toArray(function(err, result){
+		if(err) return console.log(err)
+		console.log("Pulled bookmarks");
+		bm_list = result;
+
+		console.log(bm_list);
+
+		//for each bookmark
+		for (var x =0; x<bm_list.length; x++){
+			//get url
+			var query_url = bm_list[x].bookmark_url
+			var options = {
+				uri: query_url,
+				transform: function(body){
+					return cheerio.load(body);
+				}
+			};
+
+			//pull html
+
+			rp(options)
+				.then(function(htmlString){
+					console.log(htmlString);
+				})
+				.catch(function(err){
+					console.log("link is broken");
+				});
+			//find favicon link
+			//download favicon link
+
+
+
+		};
+	});
+
+	client.close();
+	
+});
