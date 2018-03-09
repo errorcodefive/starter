9219b27d7b9a424a7f8fd1a64c656225f040e91f
Kyle Chin <kyle.chin@sterlingcapitalbrokers.com>
2018-03-09 15:11:46 -0500

    created python config file

---
+++ python_jobs/pull_favicons.py
@@ -0,0 +1,20 @@
+import urllib.request
+import pymongo
+
+from pymongo import MongoClient
+
+
+if(os.environ['NODE_ENV']='production')
+	mongo_user = os.environ['MONGO_USER']
+	mongo_pw = os.environ['MONGO_PW']
+	mongo_db = os.environ['MONGO_DB']
+else
+	
+
+
+
+client = MongoClient()
+
+
+
+urllib.request.retrieve("")
