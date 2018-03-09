65bd61eb6ed93883b8546aa1176c89313b7da4ee
Kyle Chin <kyle.chin@sterlingcapitalbrokers.com>
2018-03-09 10:42:14 -0500

    changing root

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {};
+
+config.mongodb = {};
+
+config.mongodb.user_name="startpagetest"
+config.mongodb.user_pass="startpagetest"
+config.mongodb.db_name = "startpage-test"
+module.exports = config;
