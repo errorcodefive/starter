d32869d53a38d419d0c5bcc29c164870bc4a6f6d
Kyle Chin <kyle.chin@sterlingcapitalbrokers.com>
2018-03-16 15:55:20 -0400

    logging in should be working

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var session = require('express-session');
+var bcrypt = require('bcrypt');
 
 var app = express();
 
@@ -24,14 +26,15 @@ if (process.env.NODE_ENV == "production"){
 	mongo_user = process.env.MONGO_USER;
 	mongo_pw = process.env.MONGO_PW;
 	mongo_db = process.env.MONGO_DB;
-
+	session_secret = process.env.SESSION_SECRET
 } else {
 	mongo_user = config.mongodb.user_name;
 	mongo_pw = config.mongodb.user_pass;
 	mongo_db = config.mongodb.db_name;
+	session_secret = config.sessions.secret;
 }
 
-
+var sess;
 
 
 //bodyParser
@@ -47,23 +50,55 @@ MongoClient.connect(mongo_link, (err, client)=>{
 	if (err) return console.log(err)
 	db = client.db(mongo_db);
 	console.log("Connected to MongoDB");
-
+	app.use(session({
+		secret: session_secret,
+		resave: true,
+		saveUninitialized: true,
+	}));
 	app.use(bodyParser.json());
 	app.use(bodyParser.urlencoded({extended: true}));
-
 	app.engine('html', require('ejs').renderFile);
 
 	app.set('view engine', 'ejs');
 
+
+
+
 	app.get('/', function (req, res) {
-		
-		//sending bookmarks
-		db.collection("bookmarks").find().toArray(function(err, result){{
-			if(err) throw err;
-			res.render('pages/main', {book_in: result});
-		}})
+
+		console.log("home get request");
+
+		if(req.session.authenticated){
+			console.log("it is a good session redirecting");
+			renderMain(res);
+		}else{
+			console.log("not good request: " + req.session.authenticated + " redirecting");
+			res.render("pages/login");
+		}
+
 	});
 
+
+
+	app.post('/pw', function(req,res){
+		console.log("login request received");
+
+		user_in = JSON.parse(JSON.stringify(req.body));
+		console.log("email is: " + user_in['email']);
+		
+		//check user_in password to make sure it equals the mongodb one
+		//if it does then ???
+		db.collection("users").findOne({'email':user_in['email']}, function(err, response){
+			if(err) throw err;
+			bcrypt.compare(user_in['password'], res.password_hash, function(err,response){
+				req.session.authenticated = true;
+				console.log("session is set to true: " + req.session.authenticated);
+			});
+		});
+
+		renderMain(res);
+
+	});
 
 
 	//post from bookmarks form 
@@ -80,11 +115,7 @@ MongoClient.connect(mongo_link, (err, client)=>{
 			});
 		});
 
-		db.collection("bookmarks").find().toArray(function(err, result){{
-			if(err) throw err;
-			console.log("Pulled bookmarks");
-			res.render('pages/main', {book_in: result});
-		}});
+		renderMain(res);
 
 
 	});
@@ -120,3 +151,20 @@ MongoClient.connect(mongo_link, (err, client)=>{
 	console.log('Running on http://localhost:8080');
 
 });
+
+function renderMain(res){
+	db.collection("bookmarks").find().toArray(function(err, result){{
+		console.log("redirecting to pages/main");
+		if(err) throw err;
+		res.render('pages/main', {book_in: result});
+	}});
+}
+
+function checkAuth(req, res, next){
+	console.log("checkAuth called");
+	if(!req.session.authenticated){
+		res.render('pages/login');
+		return;
+	}
+	next();
+}
