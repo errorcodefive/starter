42b0ddef47a4ff7f47228a60fc24a079dfd7ad0b
Kyle Chin <kyle.chin@sterlingcapitalbrokers.com>
2018-04-12 16:00:31 -0400

    created bookmark controller and preparing to add functions

---
+++ controllers/bookmarks.js
@@ -0,0 +1,19 @@
+var BookmarkController = function BookmarkController (bookmarkView, bookmarkModel){
+	this.bookmarkView = bookmarkView;
+	this.bookmarkModel = bookmarkModel;
+};
+
+//URL, name, favicon URI
+
+//set bookmark
+
+//get bookmark
+
+//delete bookmark
+
+//edit bookmark
+
+//generate favicon
+
+//save favicon in s3
+
